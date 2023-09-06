7d091dcc392838604b1c4006e6a0026376d8158f
thomasbuchholtzer <143712588+thomasbuchholtzer@users.noreply.github.com>
2023-09-06 11:14:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from src.utils.ask_question_to_pdf import ask_question_to_pdf
 app = Flask(__name__)
 
 # Route pour la page d'accueil
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', title='Accueil')
+    return render_template("index.html", title="Accueil")
 
 # Réponse
-@app.route('/prompt', methods=['POST'])
+@app.route("/prompt", methods=["POST"])
 def prompt():
     message = {}
-    question = request.form['prompt'] 
-    message['answer'] = ask_question_to_pdf(question)[0]
-    # message['answer'] = question + "coubehhh {:D"
+    question = request.form["prompt"] 
+    message["answer"] = ask_question_to_pdf(question)[0]
+    # message["answer"] = question + "coubehhh {:D"
     return message
 
 # Question
-@app.route('/question', methods=['GET'])
+@app.route("/question", methods=["GET"])
 def ask():
     Questioncours = {}
-    # Questioncours['answer'] = "Bonjour"
-    Questioncours['answer'] = ask_question_to_pdf("Pose-moi une question sur le document suivant")[0]
+    # Questioncours["answer"] = "Bonjour"
+    Questioncours["answer"] = ask_question_to_pdf("Pose-moi une question sur le document suivant")[0]
     return Questioncours
 
 if __name__ == '__main__':
