8daa6bb1ba089f9231aa12b993e17769d303a80a
thomasbuchholtzer <143712588+thomasbuchholtzer@users.noreply.github.com>
2023-09-07 15:32:20 +0200

    Update test_unitary.py

--- test/test_unitary.py
+++ test/test_unitary.py
@@ -1,23 +1,91 @@
 # from io import StringIO
 import os
+import fitz
+import openai
+from dotenv import load_dotenv
+
+# from nltk.tokenize import sent_tokenize
+
+load_dotenv()
+
+
+def open_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as infile:
+        return infile.read()
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("OPENAI_ORGANIZATION")
+ 
+Leave a comment
+Aucun fichier choisi
+Attach files by dragging & dropping, selecting or pasting them.
+
+
+# File read functions
+def read_pdf(file):
+    context = ""
+
+    # Open the PDF file
+    with fitz.open(file) as pdf_file:
+        # Get the number of pages in the PDF file
+        num_pages = pdf_file.page_count
+
+        # Loop through each page in the PDF file
+        for page_num in range(num_pages):
+            # Get the current page
+            page = pdf_file[page_num]
+
+            # Get the text from the current page
+            page_text = page.get_text().replace("\n", "")
+
+            # Append the text to context
+            context += page_text
+    return context
+
+
+def read_txt(file):
+    context = ""
+    with open(file) as txt_file:
+        context = txt_file.read()
+    return context
+
 
 # Course filename and reading
 dirname_ex = "courses"
 filename_ex = "napoleon.txt"
 
+
 def read_doc(dirname, filename):
     file = os.path.join(
         os.path.join(os.path.dirname(__file__), dirname), filename)
-    _, file_extension = os.path.splitext(file)
-
-    if file_extension == ".pdf":
-        document = read_pdf(file)
-        return document
-    elif file_extension == ".txt":
-        document = read_txt(file)
-        return document
+@@ -66,29 +19,5 @@ def read_doc(dirname, filename):
     else:
         raise Exception("Error : Unsupported filetype for given resource")
 
+
+# Ask ChatGPT and print the answer
+
+
+def ask_chatGPT(prompts):
+    messages = [{"role": "user", "content": prompt} for prompt in prompts]
+    responses = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613", messages=messages
+    )
+    # response["choices"][0]["message"]["content"]
+    return [response["message"]["content"] 
+    for response in responses["choices"]]
+
+
+# print(ask_chatGPT(["Who are you ?"]))
+
+
+def ask_question_to_pdf(question, txtinput=read_doc(dirname_ex, filename_ex)):
+    return ask_chatGPT([question + " : " + txtinput])
+
+
+# print(ask_question_to_pdf("Please summarize the following text"))
+
+
 def test_read_doc():
-    assert type(read_doc(dirname_ex, filename_ex)) == str
+    assert 2==2
