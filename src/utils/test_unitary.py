7abd7fa044d79fc76cfb7a0f4ec863b495215083
thomasbuchholtzer <143712588+thomasbuchholtzer@users.noreply.github.com>
2023-09-07 11:45:49 +0200

    Update test_unitary.py

--- src/utils/test_unitary.py
+++ src/utils/test_unitary.py
@@ -53,7 +53,8 @@ filename_ex = "napoleon.txt"
 
 
 def read_doc(dirname, filename):
-    file = os.path.join(os.path.join(os.path.dirname(__file__), dirname), filename)
+    file = os.path.join(
+        os.path.join(os.path.dirname(__file__), dirname), filename)
     _, file_extension = os.path.splitext(file)
 
     if file_extension == ".pdf":
@@ -75,7 +76,8 @@ def ask_chatGPT(prompts):
         model="gpt-3.5-turbo-0613", messages=messages
     )
     # response["choices"][0]["message"]["content"]
-    return [response["message"]["content"] for response in responses["choices"]]
+    return [response["message"]["content"] 
+    for response in responses["choices"]]
 
 
 # print(ask_chatGPT(["Who are you ?"]))
